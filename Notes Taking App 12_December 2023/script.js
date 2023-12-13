14fae0ee4bdc5e07c60bca380a1516be5280fc57
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-12-13 10:00:51 +0530

    Add Final Commit

--- Notes Taking App 12_December 2023/script.js
+++ Notes Taking App 12_December 2023/script.js
@@ -1,24 +1,78 @@
-
-
-
 const addButton = document.querySelector("button");
 const noteContainer = document.querySelector(".Notebox"); // Change main to noteContainer
 
+
+// ************************************************************************
+
 addButton.addEventListener("click", function () {
-    addNote();
+  addNote();
 });
 
-const addNote = function () {
-    const noteBox = document.createElement("div");
-    noteBox.classList.add("box"); // Use classList.add to add a class
-    noteBox.innerHTML = `
+// **************************************************************************
+const addNote = function (text = "") {
+  const noteBox = document.createElement("div");
+  noteBox.classList.add("box"); // Use classList.add to add a class
+  noteBox.innerHTML = `
         <div class="box-top">
-            <i class="fa-solid fa-floppy-disk"></i>
-            <i class="fa-solid fa-trash"></i>
+            <i class=" save fa-solid fa-floppy-disk"></i>
+            <i class=" trash fa-solid fa-trash"></i>
         </div>
-        <textarea name="" id=""></textarea>
+        <textarea name="" id="">${text}</textarea>
     `;
-    noteContainer.appendChild(noteBox); // Append to the existing noteContainer
+
+  noteBox.querySelector(".trash").addEventListener("click", function () {
+    noteBox.remove();
+    saveNotes();
+  });
+
+  noteBox.querySelector(".save").addEventListener("click", function () {
+    saveNotes();
+  });
+
+  noteBox.querySelector("textarea").addEventListener("focusout", function () {
+    saveNotes();
+  });
+
+  noteContainer.appendChild(noteBox); // Append to the existing noteContainer
+  saveNotes();
 };
 
+// ***********************************************************************
+const saveNotes = function () {
+    const storeNote = document.querySelectorAll(".box textarea");
+    const data = [];
+    storeNote.forEach((noteBox) => {
+      data.push(noteBox.value);
+    });
+    //    console.log(data);
+    if (data.length === 0) {
+      localStorage.removeItem("storeNote");
+    } else {
+      localStorage.setItem("storeNote", JSON.stringify(data));
+    }
+  };
+  
+// **********************************************************************
 
+(function () {
+  const savestoreNote = JSON.parse(localStorage.getItem("storeNote"));
+  if (savestoreNote === null) {
+    addNote();
+  } else {
+    savestoreNote.forEach((lsNote) => {
+      addNote(lsNote);
+    });
+  }
+})();
+
+// *********************************************************************
+// (
+//     function () {
+//         const savestoreNote = JSON.parse(localStorage.getItem("storeNote")) || [];
+//         savestoreNote.forEach(
+//             (lsNote) => {
+//                 addNote(lsNote)
+//             }
+//         );
+//     }
+// )();
