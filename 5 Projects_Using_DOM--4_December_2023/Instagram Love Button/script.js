fbef874cf4057116f9a9ed9a8038dbc213094135
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-12-14 22:05:23 +0530

    Add Some Changes

--- 5 Projects_Using_DOM--4_December_2023/Instagram Love Button/script.js
+++ 5 Projects_Using_DOM--4_December_2023/Instagram Love Button/script.js
@@ -2,6 +2,9 @@ let box = document.querySelector(".box");
 let love = document.querySelector("i");
 
 box.addEventListener("dblclick", function () {
-    love.style.transform = "translate(-50%, -50%) scale(1)";
-    love.style.opacity = 0.8;
+  love.style.transform = "translate(-50%, -50%) scale(1)";
+//   love.style.opacity = 0.8;
+  setTimeout(function () {
+    love.style.transform = "translate(-50%, -50%) scale(0)";
+  }, 1000);
 });
