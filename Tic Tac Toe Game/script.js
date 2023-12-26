0000ffb9a056e84107fe2f7d529f65b66f6a8040
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-12-26 06:23:35 +0530

    Add Some Commit

--- Tic Tac Toe Game/script.js
+++ Tic Tac Toe Game/script.js
@@ -1,5 +1,12 @@
 
 let gameover = document.querySelector("gameover.mp3")
 let music = document.querySelector("music.mp3")
-let ting = document.querySelector("ting.mp3")
+// let ting = document.querySelector("ting.mp3")
+let Audioturn = new Audio("ting.mp3")
 
+let turn = "x"
+
+//to Change the turn
+const changeTurn = function () {
+    return turn ===  "x"?"0" :"x"
+}
