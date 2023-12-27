2413f2a3a58da578ae09826d4a294b6d84cc189c
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-12-27 21:30:37 +0530

    Add Some Commit

--- Tic Tac Toe Game/script.js
+++ Tic Tac Toe Game/script.js
@@ -1,12 +1,9 @@
-
-
-
 let gameover = new Audio("./SoundContainer/gameover.mp3");
 let music = new Audio("./SoundContainer/music.mp3");
 // let Audioturn = new Audio("./SoundContainer/ting.mp3");
 
 let turn = "x";
-
+let gameover1 = false;
 // to Change the turn
 const changeTurn = function () {
   return turn === "x" ? "0" : "x";
@@ -14,7 +11,30 @@ const changeTurn = function () {
 
 // function to check for a win
 function checkwin() {
+  let boxtexts = document.getElementsByClassName("boxtext");
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   
+
+  wins.forEach(function (e) {
+    if (
+      boxtexts[e[0]].innerText === boxtexts[e[1]].innerText &&
+      boxtexts[e[2]].innerText === boxtexts[e[1]].innerText &&
+      boxtexts[e[0]].innerText !== ""
+    ) {
+      document.querySelector(".info").innerText =
+        boxtexts[e[0]].innerText + "won";
+      gameover1 = true;
+    }
+  });
 }
 
 // Game logic is starting here
@@ -28,7 +48,9 @@ Array.from(boxes).forEach(function (element) {
       turn = changeTurn();
       // Audioturn.play(); // Uncomment this if you want to play audio
       checkwin();
-      document.querySelector(".info").innerText = "Turn for " + turn; // Corrected this line
+      if (!gameover1) {
+        document.querySelector(".info").innerText = "Turn for " + turn; // Corrected this line
+      }
     }
   });
 });
